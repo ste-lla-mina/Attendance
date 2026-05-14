13a59df547eda93b8d29bb5ab820d619ff62bea8
ste-lla-mina <nsmizero10@gmail.com>
2026-05-14 07:39:39 -0800

    Added the dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Auth from './components/Auth';
+import Dashboard from './components/Dashboard';
 
 
 function App() {
   const [currentPage, setCurrentPage] = useState('auth'); 
+  const [activeSection, setActiveSection] = useState('main'); 
   const [userEmail, setUserEmail] = useState('');
 
   const handleLogin = (email) => {
@@ -14,10 +16,30 @@ function App() {
   const handleLogout = () => {
     setUserEmail(''); 
     setCurrentPage('auth');
+    setActiveSection('main');
   };
 
   if (currentPage === 'auth') {
     return <Auth onLoginSuccess={handleLogin} />;
+  }
+
+  switch (activeSection) {
+    case 'tickets':
+      return <TicketSection onBack={() => setActiveSection('main')} />;
+    case 'money':
+      return <MoneySection onBack={() => setActiveSection('main')}  />;
+    case 'phones':
+      return <PhoneSection onBack={() => setActiveSection('main')}/>;
+    case 'attendance':
+      return <Attendance onBack={()=>setActiveSection('main')}/>;
+    default:
+      return (
+        <Dashboard 
+          userEmail={userEmail} 
+          onLogout={handleLogout} 
+          onSectionClick={(sectionName) => setActiveSection(sectionName)} 
+        />
+      );
   }
 }
 
