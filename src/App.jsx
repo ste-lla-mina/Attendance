171c45a761b79c8bdd9b52f65edc68a509c2a53b
ste-lla-mina <nsmizero10@gmail.com>
2026-05-14 19:45:19 -0800

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import Auth from './components/Auth';
 import Dashboard from './components/Dashboard';
 import LessonTracking from './components/LessonTracking';
+import StudentsAttendance from './components/StudentsAttendance';
+
 
 
 function App() {
   const [currentPage, setCurrentPage] = useState('auth'); 
   const [activeSection, setActiveSection] = useState('main'); 
   const [userEmail, setUserEmail] = useState('');
-
   const handleLogin = (email) => {
     setUserEmail(email); 
     setCurrentPage('app');
@@ -28,7 +29,8 @@ function App() {
     case 'teachers':
       return <LessonTracking onBack={() => setActiveSection('main')} />;
     case 'students':
-      return <MoneySection onBack={() => setActiveSection('main')}  />;
+      return <StudentsAttendance onBack={() => setActiveSection('main')}  />;
+ 
     default:
       return (
         <Dashboard 
