49cd3801d715073c520bd128379ea80bd99e7094
ste-lla-mina <nsmizero10@gmail.com>
2026-05-15 16:21:35 -0800

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,13 @@
 import React, { useState } from 'react';
-import Auth from './components/Auth';
-import Dashboard from './components/Dashboard';
-import LessonTracking from './components/LessonTracking';
-import StudentsAttendance from './components/StudentsAttendance';
+import ReturnSection from './sn/ReturningDay';
 
 
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('auth'); 
-  const [activeSection, setActiveSection] = useState('main'); 
-  const [userEmail, setUserEmail] = useState('');
-  const handleLogin = (email) => {
-    setUserEmail(email); 
-    setCurrentPage('app');
-  };
-
-  const handleLogout = () => {
-    setUserEmail(''); 
-    setCurrentPage('auth');
-    setActiveSection('main');
-  };
-
-  if (currentPage === 'auth') {
-    return <Auth onLoginSuccess={handleLogin} />;
-  }
-
-  switch (activeSection) {
-    case 'teachers':
-      return <LessonTracking onBack={() => setActiveSection('main')} />;
-    case 'students':
-      return <StudentsAttendance onBack={() => setActiveSection('main')}  />;
- 
-    default:
+  
       return (
-        <Dashboard 
-          userEmail={userEmail} 
-          onLogout={handleLogout} 
-          onSectionClick={(sectionName) => setActiveSection(sectionName)} 
+        <ReturnSection
         />
       );
   }
-}
-
 export default App;
